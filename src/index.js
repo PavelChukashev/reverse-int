f914cd4f8841ad85582afb741b4723a80b54a400
PavelChukashev <chukashov93@gmail.com>
2020-03-22 17:04:19 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+
+
+    let str = '' + n;
+    let splitedStr = str.split('');
+    let reversedArr = splitedStr.reverse();
+    let joinedArr = reversedArr.join('');
+    let num = Number(joinedArr);
+
+
+    console.log(joinedArr);
+    return joinedArr;
+    
+    
+    
+};
